e7ef8bd3590cc96943759654862c42cbc92f577d
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-10-16 18:14:24 -0700

    Added filter functionality for searchField

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      )
     return (
       <div className="App">
       <input 
@@ -30,7 +34,7 @@ class App extends Component {
           this.setState({ searchField: e.target.value})
         }}
       />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
